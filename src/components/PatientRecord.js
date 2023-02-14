f4570944de9bb670a1808c1f7ba8c36201b770d8
Tiffany Tran <tif.tran86@gmail.com>
2023-02-13 18:59:31 -0800

    vaccine details and name showing up

--- src/components/PatientRecord.js
+++ src/components/PatientRecord.js
@@ -7,33 +7,32 @@ const PatientRecord = (props) => {
   const [vaccineDetail, setVaccineDetail] = useState([]);
 
   // DO NOT DELETE!
-  const getPatientRecords = async (id) => {
-    const data = await getVaccineRecordAPI(props.patientDetail.id);
-    setVaccineRecord(data);
+  // const getPatientRecords = async (id) => {
+  //   const data = await getVaccineRecordAPI(props.patientDetail.id);
+  //   setVaccineRecord(data);
 
-    const newdata = data.reduce(function (filtered, option) {
-      if (option.vaccine_id) {
-        return filtered.concat([option.vaccine_id]);
-      }
-      return filtered;
-    }, []);
+  //   const newdata = data.reduce(function (filtered, option) {
+  //     if (option.vaccine_id) {
+  //       return filtered.concat([option.vaccine_id]);
+  //     }
+  //     return filtered;
+  //   }, []);
 
-    await getVaccineDetail(newdata);
-  };
+  //   await getVaccineDetail(newdata);
+  // };
 
-  const getVaccineDetail = async (id) => {
-    // return await getPatientVaccineDetailAPI(id);
+  // const getVaccineDetail = async (id) => {
+  //   // return await getPatientVaccineDetailAPI(id);
 
-    const data = await getPatientVaccineDetailAPI(id); // DO NOT DELETE
+  //   const data = await getPatientVaccineDetailAPI(id); // DO NOT DELETE
 
-    const vaccineDetailsMap = {};
-    for (let i = 0; i < data.length; i++) {
-      const entry = data[i];
-      vaccineDetailsMap[entry.id] = entry;
-    }
-    await setVaccineDetail(vaccineDetailsMap);
-    console.log(vaccineDetail);
-  };
+  //   const vaccineDetailsMap = {};
+  //   for (let i = 0; i < data.length; i++) {
+  //     const entry = data[i];
+  //     vaccineDetailsMap[entry.id] = entry;
+  //   }
+  //   await setVaccineDetail(vaccineDetailsMap);
+  // };
 
   // const vaccineDetailsMap = (data) => {
   //   for (let i = 0; i < data.length; i++) {
@@ -51,7 +50,44 @@ const PatientRecord = (props) => {
   // };
 
   useEffect(() => {
-    getPatientRecords();
+    const getVaccineDetail = async (id) => {
+      // return await getPatientVaccineDetailAPI(id);
+
+      const data = await getPatientVaccineDetailAPI(id); // DO NOT DELETE
+
+      const vaccineDetailsMap = {};
+      for (let i = 0; i < data.length; i++) {
+        const entry = data[i];
+        vaccineDetailsMap[entry.id] = entry;
+      }
+      return vaccineDetailsMap;
+    };
+
+    const getVaccineIds = async (records) => {
+      const newdata = records.reduce(function (filtered, option) {
+        if (option.vaccine_id) {
+          return filtered.concat([option.vaccine_id]);
+        }
+        return filtered;
+      }, []);
+
+      return newdata;
+    };
+
+    const getPatientRecords = async (id) => {
+      const data = await getVaccineRecordAPI(id);
+      return data;
+    };
+
+    const getPatientVaccineDetails = async (id) => {
+      const patientRecords = await getPatientRecords(id);
+      const patientVaccineIds = await getVaccineIds(patientRecords);
+      const patientVaccineDetail = await getVaccineDetail(patientVaccineIds);
+      await setVaccineRecord(patientRecords);
+      await setVaccineDetail(patientVaccineDetail);
+    };
+
+    getPatientVaccineDetails(props.patientDetail.id);
   }, [props.patientDetail]);
 
   // console.log("printing vaccine records");
@@ -62,15 +98,15 @@ const PatientRecord = (props) => {
   // for each entry we use the vaccine_id to look up into the vaccineDetail
   // now we have complete information about the administered date and the vaccine itself
 
-  const getVaccinesArray = (data) => {
-    return data.map((patient) => (
+  const getVaccinesArray = (records, vaccines) => {
+    return records.map((record) => (
       <Thing
-        key={patient.id}
-        id={patient.id}
-        date_administered={patient.date_administered}
-        vaccine_id={patient.vaccine_id}
-        patient_id={patient.patient_id}
-        // vaccine={vaccines[patient.vaccine_id]}
+        key={record.id}
+        id={record.id}
+        date_administered={record.date_administered}
+        vaccine_id={record.vaccine_id}
+        patient_id={record.patient_id}
+        vaccine={vaccines[record.vaccine_id]}
       />
     ));
   };
